60b57aed076c24faaec9202c75178e08669551c4
Sanket S Suradkar <68096112+SanketSuradkar@users.noreply.github.com>
2025-10-13 16:49:40 +0530

    Update admin.py

--- crudapp/admin.py
+++ crudapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Student
-
+# this admin code is usend to add the  emial name and the id of the perticula targated student and the customer or the end user 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ["id", "name", "email"]
+
